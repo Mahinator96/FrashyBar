1f890a4ab072f969d97090a59096272b10ea1154
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-07-20 18:19:47 +0300

    Готов день 3 - сделано модальное окно корзины

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ const getData = async () => {
 const createCard = (item) => {
 	const cocktail = document.createElement('article');
 	cocktail.classList.add('cocktail');
-	console.log(item);
 
 	cocktail.innerHTML = 
 	`
@@ -29,7 +28,52 @@ const createCard = (item) => {
 	return cocktail;
 }
 
+const modalController = ({modal, btnOpen, time = 300}) => {
+	const buttonElem = document.querySelector(btnOpen);
+	const modalElem = document.querySelector(modal);
+
+	modalElem.style.cssText = 
+`
+		display: flex;
+		visibility: hidden;
+		opacity: 0;
+		transition: opacity ${time}ms ease-in-out;
+	`
+
+	const closeModal = (event) => {
+		const target = event.target;
+		const code = event.code;
+
+		if(target === modalElem || code === "Escape") {
+			modalElem.style.opacity = '0';
+	
+			setTimeout(() => {
+				modalElem.style.visibility = 'hidden';
+			}, time)
+
+			window.removeEventListener('keydown', closeModal);
+		}
+	}
+
+	const openModal = () => {
+		modalElem.style.visibility = 'visible';
+		modalElem.style.opacity = '1';
+
+		window.addEventListener('keydown', closeModal);
+	}
+	
+	buttonElem.addEventListener('click', openModal);
+	modalElem.addEventListener('click', closeModal);
+
+	return {openModal, closeModal}
+}
+
 const init = async () => {
+	modalController({
+		modal: '.modal_order', 
+		btnOpen: '.header__btn-order'
+	});
+
 	const goodsListElem = document.querySelector(".goods__list");
 	const data = await getData();
 
@@ -42,7 +86,6 @@ const init = async () => {
 	})
 
 	goodsListElem.append(...cardsCocktail);
-	// console.log(data);
 }
 
 init()
