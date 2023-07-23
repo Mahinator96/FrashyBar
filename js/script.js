1571d41b6e8a3f1be5a372ffae81dd45fbb30629
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-07-23 20:09:45 +0300

    Готова отправка карзины

--- js/script.js
+++ js/script.js
@@ -11,14 +11,42 @@ const price = {
 	Лёд: 10,
 	Биоразлагаемый: 20,
 	Пластиковый: 0,
-}
+};
+
+const cartDataControl = {
+	get() {
+
+
+		return JSON.parse(localStorage.getItem('freshyBarCart') || '[]')
+	},
+	add(item) {
+		const cartData = this.get();
+		item.idls = Math.random().toString(36).substring(2, 8);
+
+		cartData.push(item);
+		localStorage.setItem('freshyBarCart', JSON.stringify(cartData));
+	},
+	remove(idls) {
+		const cartData = this.get();
+		const index = cart.findIndex((item => item.idls === idls));
+
+		if (index !== -1) {
+			cartData.splice(index, 1);
+		}
+
+		localStorage.setItem('freshyBarCart', JSON.stringify(cartData));
+	},
+	clear() {
+		localStorage.removeItem('freshyBarCart');
+	},
+};
 
 const getData = async () => {
 	const response = await fetch(`${API_URL}api/goods`);
 	const data = await response.json();
 
 	return data;
-}
+};
 
 const createCard = (item) => {
 	const cocktail = document.createElement('article');
@@ -39,7 +67,7 @@ const createCard = (item) => {
 	`;
 
 	return cocktail;
-}
+};
 
 const scrollService = {
 	scrollPosition: 0,
@@ -54,22 +82,23 @@ const scrollService = {
 			left: 0;
 			height: 100vh;
 			width: 100vw;
-			padding-rigth: ${window.innerWidth - document.body.offsetWidth}px;
+			padding-right: ${window.innerWidth - document.body.offsetWidth}px;
 		`;
 	},
 	enabledScroll() {
 		document.body.style.cssText = '';
+		
 		window.scroll({ top: this.scrollPosition});
 		document.documentElement.style.scrollBehavior = 'smooth';
 	}
-}
+};
 
-const modalController = ({modal, btnOpen, time = 300}) => {
+const modalController = ({modal, btnOpen, time = 300, open, close}) => {
 	const buttonElems = document.querySelectorAll(btnOpen);
 	const modalElem = document.querySelector(modal);
 
 	modalElem.style.cssText = 
-`
+	`
 		display: flex;
 		visibility: hidden;
 		opacity: 0;
@@ -80,19 +109,27 @@ const modalController = ({modal, btnOpen, time = 300}) => {
 		const target = event.target;
 		const code = event.code;
 
-		if(target === modalElem || code === "Escape") {
+		if(event === 'close' || target === modalElem || code === "Escape") {
 			modalElem.style.opacity = '0';
 			
 			setTimeout(() => {
-				modalElem.style.visibility = 'hidden';
 				scrollService.enabledScroll();
+				modalElem.style.visibility = 'hidden';
+
+				if (close) {
+					close();
+				}
 			}, time)
 
 			window.removeEventListener('keydown', closeModal);
 		}
 	}
 
-	const openModal = () => {
+	const openModal = (e) => {
+		if (open) {
+			open({btn: e.target});
+		}
+
 		modalElem.style.visibility = 'visible';
 		modalElem.style.opacity = '1';
 
@@ -104,9 +141,11 @@ const modalController = ({modal, btnOpen, time = 300}) => {
 	})
 	
 	modalElem.addEventListener('click', closeModal);
+	modalElem.closeModal = closeModal;
+	modalElem.openModal = openModal;
 
 	return {openModal, closeModal}
-}
+};
 
 const getFormData = (form) => {
 	const formData = new FormData(form);
@@ -125,13 +164,11 @@ const getFormData = (form) => {
 	}
 
 	return data;
-}
+};
 
 const calculateTotalPrice = (form, startPrice) => {
 	let totalPrice = startPrice;
-
 	const data = getFormData(form);
-	console.log(data);
 
 	if (Array.isArray(data.ingredients)) {
 		data.ingredients.forEach(item => {
@@ -150,38 +187,203 @@ const calculateTotalPrice = (form, startPrice) => {
 	}
 
 	totalPrice += price[data.cup] || 0;
-	console.log(totalPrice);
 
 	return totalPrice;
+};
+
+const formControl = (form, cb) => {
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const data = getFormData(form);
+		cartDataControl.add(data);
+
+		if (cb) {
+			cb();
+		}
+	})
 }
 
 const calculateMakeYourOwn = () => {
-	const formMakeOwn = document.querySelector('.make__form_make-yuor-own');
-	const makeInputPrice = formMakeOwn.querySelector('.make__input-price');
-	const makeTotalPrice = formMakeOwn.querySelector('.make__total-price');
+	const modalMakeOwn = document.querySelector('.modal_make-your-own');
+	const formMakeOwn = modalMakeOwn.querySelector('.make__form_make-yuor-own');
+	const makeInputTitle = modalMakeOwn.querySelector('.make__input-title');
+	const makeInputPrice = modalMakeOwn.querySelector('.make__input-price');
+	const makeTotalPrice = modalMakeOwn.querySelector('.make__total-price');
+	const makeAddBtn = modalMakeOwn.querySelector('.make__add-btn');
 
 	const handlerChange = () => {
 		const totalPrice = calculateTotalPrice(formMakeOwn, 150);
-		
+		const data = getFormData(formMakeOwn);
+
+		if (data.ingredients) {
+			const ingredients = Array.isArray(data.ingredients) 
+				? data.ingredients.join(', ')
+				: data.ingredients;
+
+			makeInputTitle.value = `Конструктор: ${ingredients}`;
+			makeAddBtn.disabled = false;
+		} else {
+			makeAddBtn.disabled = true;
+		}
+
 		makeInputPrice.value = totalPrice;
-		makeTotalPrice.textContent = `${totalPrice} ₽`;
+		makeTotalPrice.innerHTML = `${totalPrice}&nbsp;₽`;
 	}
 
 	formMakeOwn.addEventListener('change', handlerChange);
+	formControl(formMakeOwn, () => {
+		modalMakeOwn.closeModal('close');
+	});
 	handlerChange();
+
+	const resetForm = () => {
+		makeTotalPrice.innerHTML = '';
+		makeAddBtn.disabled = true;
+
+		formMakeOwn.reset();
+	}
+
+	return {resetForm};
+};
+
+const calculateAdd = () => {
+	const modalAdd = document.querySelector('.modal_add');
+	const formAdd = modalAdd.querySelector('.make__form_add');
+	const makeTitle = modalAdd.querySelector('.make__title');
+	const makeInputTitle = modalAdd.querySelector('.make__input-title');
+	const makeInputStartPrice = modalAdd.querySelector('.make__input-start-price');
+	const makeInputPrice = modalAdd.querySelector('.make__input-price');
+	const makeInputSize = modalAdd.querySelector('.make__input-size');
+	const makeTotalPrice = modalAdd.querySelector('.make__total-price');
+	const makeTotalSize = modalAdd.querySelector('.make__total-size');
+
+	const handlerChange = () => {
+		const totalPrice = calculateTotalPrice(formAdd, +makeInputStartPrice.value);
+		makeInputPrice.value = totalPrice;
+		makeTotalPrice.innerHTML = `${totalPrice}&nbsp;₽`;
+	};
+
+	formAdd.addEventListener('change', handlerChange);
+	formControl(formAdd, () => {
+		modalAdd.closeModal('close');
+	})
+
+	const fillInForm = data => {
+		makeInputTitle.value = data.title;
+		makeTitle.textContent = data.title;
+		makeInputStartPrice.value = data.price;
+		makeInputPrice.value = data.price;
+		makeTotalPrice.innerHTML = `${data.price}&nbsp;₽`;
+		makeInputSize.value = data.size;
+		makeTotalSize.textContent = data.size;
+		handlerChange
+	};
+
+	const resetForm = () => {
+		makeTitle.textContent = '';
+		makeTotalPrice.innerHTML = '';
+		makeTotalSize.textContent = '';
+
+		formAdd.reset();
+	}
+
+	return {fillInForm, resetForm};
 }
+
+const createCartItem = (item) => {
+	const li = document.createElement('li');
+	li.classList.add('order__item');
+	li.innerHTML = 
+		`
+			<img class="order__img" src="img/cup-1.jpg" width="70" height="82" alt="${item.title}">
+
+			<div class="order__info">
+				<h3 class="order__name">${item.title}</h3>
+
+				<ul class="order__topping-list">
+					<li class="order__topping-item">${item.size}</li>
+					<li class="order__topping-item">${item.cup}</li>
+					${item.topping 
+						?(Array.isArray(item.topping)
+							? item.topping.map(topping => `<li class="order__topping-item">${topping}</li>`)
+							: `<li class="order__topping-item">${item.topping}</li>`)
+						: ''
+					}
+				</ul>
+			</div>
+
+			<button class="order__item-delete" data-idls="${item.idls}" aria-label="Удалить коктейль из корзины"></button>
+
+			<p class="order__item-price">${item.price}&nbsp;₽</p>
+		`
+
+		return li;
+};
+
+const renderCart = () => {
+	const modalOrder = document.querySelector('.modal_order');
+	const orderCount = modalOrder.querySelector('.order__count');
+	const orderList = modalOrder.querySelector('.order__list');
+	const orderTotalPrice = modalOrder.querySelector('.order__price-total');
+	const orderForm = modalOrder.querySelector('.order__form');
+
+	const orderListData = cartDataControl.get();
+
+	orderList.textContent = '';
+	orderCount.textContent = `(${orderListData.length})`;
+
+	orderListData.forEach(item => {
+		orderList.append(createCartItem(item));
+	});
+
+	orderTotalPrice.innerHTML = 
+	`${orderListData.reduce((acc, item) => acc + +item.price, 0)}&nbsp;₽`;
+
+	orderForm.addEventListener('submit', async (e) => {
+		e.preventDefault();
+
+		if (!orderListData.length) {
+			alert('Корзина пустая');
+			modalOrder.closeModal('close');
+			return;
+		}
+
+		const data = getFormData(orderForm);
+		const response = await fetch(`${API_URL}api/order`, {
+			method: 'POST',
+			body: JSON.stringify({
+				...data,
+				products: orderListData,
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			},
+		});
+
+		const {message} = await response.json();
+
+		alert(message);
+
+		cartDataControl.clear();
+		orderForm.reset();
+		modalOrder.closeModal('close');
+	})
+};
 
 const init = async () => {
 	modalController({
 		modal: '.modal_order', 
-		btnOpen: '.header__btn-order'
+		btnOpen: '.header__btn-order',
+		open: renderCart,
 	});
 	
-	calculateMakeYourOwn();
+	const { resetForm: resetFormMakeYourOwn } = calculateMakeYourOwn();
 
 	modalController({
 		modal: '.modal_make-your-own',
-		btnOpen: '.cocktail__btn_make'
+		btnOpen: '.cocktail__btn_make',
+		close: resetFormMakeYourOwn,
 	});
 	
 	const goodsListElem = document.querySelector(".goods__list");
@@ -197,10 +399,18 @@ const init = async () => {
 
 	goodsListElem.append(...cardsCocktail);
 
+	const { fillInForm: fillInFormAdd, resetForm: resetFormAdd } = calculateAdd();
+
 	modalController({
 		modal: ".modal_add",
-		btnOpen: ".cocktail__btn_add"
+		btnOpen: ".cocktail__btn_add",
+		open({btn}) {
+			const id = btn.dataset.id;
+			const item = data.find(item => item.id.toString() === id);
+			fillInFormAdd(item);
+		},
+		close: resetFormAdd,
 	})
-}
+};
 
 init()
